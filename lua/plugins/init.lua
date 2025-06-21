ee33f7fbe00f27a48d016af6778426b8f29ea54b
ibwmahin <ibwmahin@gamil.com>
2025-06-21 22:12:31 +0600

    fast update

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,12 +1,12 @@
 return {
-   -- These are some examples, uncomment them if you want to see them work!
+  -- These are some examples, uncomment them if you want to see them work!
   {
     "neovim/nvim-lspconfig",
     config = function()
       require "configs.lspconfig"
     end,
   },
--- which key nvimm 
+  -- which key nvim  gose from here
   {
     "folke/which-key.nvim",
     config = function()
