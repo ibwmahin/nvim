7d16ee6f7b854445054da0c7861c5b5be72c61c2
ibwmahin <ibwmahin@gamil.com>
2025-06-21 22:11:55 +0600

    fast update

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,37 +1,12 @@
 return {
-  -- mini map
-
-  {
-    "wfxr/minimap.vim",
-    build = "cargo install --locked code-minimap",
-    cmd = { "Minimap", "MinimapToggle", "MinimapRefresh", "MinimapClose" },
-    init = function()
-      vim.g.minimap_width = 10
-      vim.g.minimap_auto_start = 1
-      vim.g.minimap_auto_start_win_enter = 1
-      vim.g.minimap_highlight_range = 1
-      vim.g.minimap_git_colors = 1
-    end,
-  },
-
-  vim.api.nvim_create_autocmd("BufEnter", {
-    pattern = "*",
-    command = "silent! Minimap",
-  }),
-  {
-    "stevearc/conform.nvim",
-    -- event = 'BufWritePre', -- uncomment for format on save
-    opts = require "configs.conform",
-  },
-
-  -- These are some examples, uncomment them if you want to see them work!
+   -- These are some examples, uncomment them if you want to see them work!
   {
     "neovim/nvim-lspconfig",
     config = function()
       require "configs.lspconfig"
     end,
   },
-
+-- which key nvimm 
   {
     "folke/which-key.nvim",
     config = function()
