ff957238231fb434b8a69315c07db5c9593f651f
ibwmahin <ibwmahin@gamil.com>
2025-06-22 18:22:51 +0600

    fast update

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,81 +1,12 @@
 return {
-
-  -- Disable modern explorers
-  { "stevearc/oil.nvim", enabled = false },
-  { "nvim-telescope/telescope-file-browser.nvim", enabled = false },
-  { "echasnovski/mini.files", enabled = false },
-
-  -- Only nvim-tree explorer
-  {
-    "nvim-tree/nvim-tree.lua",
-    dependencies = {
-      "nvim-tree/nvim-web-devicons",
-    },
-    config = function()
-      require("nvim-tree").setup {
-        view = {
-          width = 30,
-          side = "left",
-        },
-        renderer = {
-          icons = {
-            show = {
-              folder = true,
-              file = true,
-            },
-          },
-        },
-        git = {
-          enable = true,
-        },
-      }
-
-      -- Keymap to toggle
-      vim.keymap.set("n", "<C-n>", ":NvimTreeToggle<CR>", { desc = "Toggle File Tree" })
-
-      -- Open nvim-tree when nvim is launched with a directory
-      vim.api.nvim_create_autocmd("VimEnter", {
-        callback = function()
-          local arg = vim.fn.argv(0)
-          if arg ~= "" and vim.fn.isdirectory(arg) == 1 then
-            require("nvim-tree.api").tree.open()
-          end
-        end,
-      })
-    end,
-    cmd = { "NvimTreeToggle", "NvimTreeFocus" },
-  },
-
-  -- Snacks.nvim (explorer disabled)
-  {
-    "folke/snacks.nvim",
-    priority = 1000,
-    lazy = false,
-    opts = {
-      dashboard = { enabled = true },
-      notifier = { enabled = true },
-      terminal = { enabled = true },
-      explorer = { enabled = false },
-      indent = { enabled = true },
-      input = { enabled = true },
-      picker = { enabled = true },
-      quickfile = { enabled = true },
-      scroll = { enabled = true },
-      statuscolumn = { enabled = true },
-      words = { enabled = true },
-      bigfile = { enabled = true },
-      scope = { enabled = true },
-    },
-  },
-
-  -- Add the rest of your plugins below:
+  -- These are some examples, uncomment them if you want to see them work!
   {
     "neovim/nvim-lspconfig",
     config = function()
       require "configs.lspconfig"
     end,
   },
-
+  -- which key nvim  gose from here
   {
     "folke/which-key.nvim",
     config = function()
@@ -94,13 +25,14 @@ return {
           align = "center",
         },
         triggers_blacklist = {
+          -- prevent conflicts with native bindings
           i = { "j", "k" },
           v = { "j", "k" },
         },
       }
     end,
   },
-
+  --noice.nvim using here
   {
     "folke/noice.nvim",
     event = "VeryLazy",
@@ -113,11 +45,11 @@ return {
         lsp = {
           hover = {
             enabled = true,
-            silent = true,
-            view = "hover",
+            silent = true, -- prevents focus stealing
+            view = "hover", -- optional: uses normal hover popup
           },
           signature = {
-            enabled = false,
+            enabled = false, -- turn off function signature popups while typing
           },
           override = {
             ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
@@ -141,12 +73,40 @@ return {
       vim.notify = require "notify"
     end,
   },
-
+  -- Plugin manager
   { "folke/lazy.nvim" },
 
+  -- Colorschemes
   { "folke/tokyonight.nvim", lazy = false, priority = 1000 },
   { "catppuccin/nvim", name = "catppuccin", lazy = false, priority = 1000 },
 
+  -- Dashboard, Terminal, Notifications
+
+  {
+    "folke/snacks.nvim",
+    priority = 1000, -- load early for dashboard UI
+    lazy = false, -- load immediately on start
+    opts = {
+      dashboard = { enabled = true },
+      notifier = { enabled = true },
+      terminal = { enabled = true },
+      explorer = { enabled = false },
+      indent = { enabled = true },
+      input = { enabled = true },
+      picker = { enabled = true },
+      quickfile = { enabled = true },
+      scroll = { enabled = true },
+      statuscolumn = { enabled = true },
+      words = { enabled = true },
+      bigfile = { enabled = true },
+      scope = { enabled = true },
+    },
+  },
+
+  -- Keybinding popup
+  { "folke/which-key.nvim", config = true },
+
+  -- Fuzzy Finder + fzf
   {
     "nvim-telescope/telescope.nvim",
     dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope-fzf-native.nvim" },
@@ -156,6 +116,7 @@ return {
     end,
   },
 
+  -- Treesitter
   {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
@@ -166,24 +127,26 @@ return {
   },
   { "nvim-treesitter/nvim-treesitter-textobjects", after = "nvim-treesitter" },
 
+  -- LSP + Mason
   {
-    "williamboman/mason.nvim",
-    config = true,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = true,
-  },
-  {
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-    config = true,
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+      "WhoIsSethDaniel/mason-tool-installer.nvim",
+    },
+    config = function()
+      require "configs.lspconfig"
+    end,
   },
 
+  -- Autocompletion
   {
     "hrsh7th/nvim-cmp",
     dependencies = {
       "L3MON4D3/LuaSnip",
       "saadparwaiz1/cmp_luasnip",
+      "hrsh7th/cmp-nvim-lsp",
       "hrsh7th/cmp-nvim-lsp",
       "hrsh7th/cmp-buffer",
       "hrsh7th/cmp-path",
@@ -196,6 +159,7 @@ return {
     end,
   },
 
+  -- Formatting
   {
     "stevearc/conform.nvim",
     event = "BufWritePre",
@@ -204,17 +168,23 @@ return {
     end,
   },
 
+  -- Linting
+
   {
     "mfussenegger/nvim-lint",
     event = { "BufReadPre", "BufNewFile" },
     config = function()
       local lint = require "lint"
+
       lint.linters_by_ft = {
         javascript = { "eslint" },
         typescript = { "eslint" },
         python = { "flake8" },
         lua = { "luacheck" },
+        -- add more as needed
       }
+
+      -- Run lint on save
       vim.api.nvim_create_autocmd({ "BufWritePost" }, {
         callback = function()
           require("lint").try_lint()
@@ -223,21 +193,67 @@ return {
     end,
   },
 
+  -- File Explorer
+  -- {
+  --   "nvim-tree/nvim-tree.lua",
+  --   dependencies = "nvim-tree/nvim-web-devicons",
+  --   cmd = { "NvimTreeToggle", "NvimTreeFocus" },
+  --   config = function()
+  --     require("nvim-tree").setup {
+  --       on_attach = function(bufnr)
+  --         local api = require "nvim-tree.api"
+  --         local opts = { buffer = bufnr, noremap = true, silent = true }
+  --         vim.keymap.set("n", "<C-h>", api.node.navigate.parent, opts)
+  --         vim.keymap.set("n", "<C-l>", api.node.open.edit, opts)
+  --         vim.keymap.set("n", "<C-j>", api.node.navigate.sibling.next, opts)
+  --         vim.keymap.set("n", "<C-k>", api.node.navigate.sibling.prev, opts)
+  --       end,
+  --     }
+  --   end,
+  -- },
+
+  -- Git Integration
   { "lewis6991/gitsigns.nvim", event = "BufReadPre", config = true },
+
+  -- Search & Replace
   { "MagicDuck/grug-far.nvim", config = true },
   { "folke/flash.nvim", event = "VeryLazy", opts = {} },
+
+  -- Diagnostics UI
   { "folke/trouble.nvim", dependencies = "nvim-tree/nvim-web-devicons", config = true },
+
+  -- Statusline
   { "nvim-lualine/lualine.nvim", config = true },
+
   {
-    "echasnovski/mini.comment",
+    "nvim-lualine/lualine.nvim",
     event = "VeryLazy",
-    config = true,
+    dependencies = { "nvim-tree/nvim-web-devicons" }, -- optional for icons
+    config = function()
+      require("lualine").setup {
+        options = {
+          theme = "auto", -- auto picks your colorscheme
+          section_separators = "", -- clean separators
+          component_separators = "",
+          globalstatus = true, -- one statusline for all windows (Neovim 0.7+)
+        },
+        sections = {
+          lualine_a = { "mode" },
+          lualine_b = { "branch", "diff", "diagnostics" },
+          lualine_c = { "filename", "lsp_progress" },
+          lualine_x = { "encoding", "fileformat", "filetype" },
+          lualine_y = { "progress" },
+          lualine_z = { "location" },
+        },
+      }
+    end,
   },
-  {
-    "echasnovski/mini.ai",
-    event = "VeryLazy",
-    config = true,
-  },
+
+  -- Commenting & Text Objects
+  { "echasnovski/mini.comment", event = "VeryLazy", config = true },
+  { "echasnovski/mini.ai", event = "VeryLazy", config = true },
+
+  -- Smooth Cursor + Smooth Scrolling
   {
     "gen740/SmoothCursor.nvim",
     event = "VeryLazy",
@@ -264,6 +280,8 @@ return {
       require("neoscroll").setup { easing_function = "cubic" }
     end,
   },
+
+  -- Breadcrumbs
   { "SmiteshP/nvim-navic", dependencies = "neovim/nvim-lspconfig", lazy = true, config = true },
   {
     "utilyre/barbecue.nvim",
@@ -271,10 +289,24 @@ return {
     dependencies = { "nvim-tree/nvim-web-devicons", "SmiteshP/nvim-navic" },
     config = true,
   },
+
+  -- Tailwind CSS color preview
   { "roobert/tailwindcss-colorizer-cmp.nvim", config = true },
+
+  -- AI Autocomplete (free)
+  -- {
+  --   "Exafunction/codeium.vim",
+  --   event = "InsertEnter",
+  --   config = function()
+  --     vim.g.codeium_disable_bindings = 1
+  --     vim.keymap.set("i", "<C-CR>", function()
+  --       return vim.fn["codeium#Accept"]()
+  --     end, { expr = true })
+  --   end,
+  -- },
+  -- test new blink
   { import = "nvchad.blink.lazyspec" },
 
-  -- Treesitter lang packs
   {
     "nvim-treesitter/nvim-treesitter",
     opts = {
