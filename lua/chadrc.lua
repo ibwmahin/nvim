a7f44df32686e7b6c93c0aae6d5a0c8880440581
ibwmahin <ibwmahin@gamil.com>
2025-07-01 10:19:51 +0600

    fast update

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -2,7 +2,7 @@
 local M = {}
 
 M.base46 = {
-  theme = "solarized_osaka",
+  theme = "tokyodark",
   hl_override = require("custom.highlights").override,
   -- hl_override = {
   --   Comment = { italic = true },
